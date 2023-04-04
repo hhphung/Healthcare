bb2d037a7df44bc1b760a81cc63580855037ac5f
hoi phung <hhphung@iastate.edu>
2023-04-03 23:17:55 -0500

    clearn

---
+++ frontend/healthcare/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { LoginPage } from './Login/Login';
+import { SignUpPage } from './SignUp/SignUp';
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  return (
+    <div className="App">
+      {
+        currentForm === "login" ? <LoginPage onFormSwitch={toggleForm} /> : <SignUpPage onFormSwitch={toggleForm} />
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/healthcare/src/SignUp/SignUp.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import './SignUp.css';
+
+export const SignUpPage = (props) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('First Name:', firstName);
+    console.log('Last Name:', lastName);
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Sign Up</h1>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Sign Up</button>
+        <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Back to Login</button>
+      </form>
+    </div>
+  );
+}
+
