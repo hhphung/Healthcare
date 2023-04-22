e21011495e4ceda2b309e55717bb336b217b95e1
hoi phung <hhphung@iastate.edu>
2023-04-21 23:08:06 -0500

    add homepage

--- frontend/healthcare/src/dashboard/dashboard.jsx
+++ frontend/healthcare/src/dashboard/dashboard.jsx
@@ -1,10 +1,23 @@
-import React, { useState } from 'react';
-
-
+import React from 'react';
+import './dashboard.css';
+import Navbar from './navbar/Navbar.js';
+import { BrowserRouter as Router,  Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports.js';
+import Products from './pages/Products.js';
 
 export const DashBoardPage = (props) => {
   return (
-    <h1>Hoi</h1>
+    <>
+      <Router>
+        <Navbar />
+        < Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
